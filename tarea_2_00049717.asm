75b0d3e55ae5cd10ffbd99f8452f8ba23bc90a9e
estherlaraaaa <37164243+estherlaraaaa@users.noreply.github.com>
2020-04-23 22:37:34 -0600

    ejercicio 1 terminado

--- tarea_2_00049717.asm
+++ tarea_2_00049717.asm
@@ -14,4 +14,23 @@
         mov     cl, 5d
         div     cl
 
-    
+        mov     cl, "P"
+        mov [200h], cl
+        mov     cl, "e"
+        mov [201h], cl
+        mov     cl, "o"
+        mov [202h], cl
+        mov     cl, "r"
+        mov [203h], cl
+        
+        mov     cl, ""
+        mov [204h], cl
+        
+        mov     cl, "n"
+        mov [205h], cl
+        mov     cl, "a"     
+        mov [206h], cl
+        mov     cl, "d"
+        mov [207h], cl
+        mov     cl, "a"
+        mov [208h], cl
