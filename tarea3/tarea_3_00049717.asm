0bea3e63d63b8f1ad70a063a30b74d345c099c1c
estherlaraaaa <37164243+estherlaraaaa@users.noreply.github.com>
2020-05-18 21:15:13 -0600

    arreglando identacion que se desconfiguro

--- tarea3/tarea_3_00049717.asm
+++ tarea3/tarea_3_00049717.asm
@@ -1,95 +1,95 @@
- org     100h
+                org     100h
 
-section .text
+                section .text
 
-    call    setup
-	call 	texto	
-	call 	cursor
-	call 	linea1
-    call 	linea2
-    call 	linea3
-	call	kbwait
+                call    setup
+                call 	texto	
+                call 	cursor
+                call 	linea1
+                call 	linea2
+                call 	linea3
+                call	kbwait
 
-	int 	20h
+                int 	20h
 
-setup:  mov     al, 7d
-        mov     [202h], al
-        mov     al, 00001111b
-        mov     [210h], al
-        mov     al, 00000111b
-        mov     [211h], al
-        mov     al, 00001110b
-        mov     [212h], al
-        mov     ax, 0000h
-        mov     [200h], ax
-        ret
+setup:          mov     al, 7d
+                mov     [202h], al
+                mov     al, 00001111b
+                mov     [210h], al
+                mov     al, 00000111b
+                mov     [211h], al
+                mov     al, 00001110b
+                mov     [212h], al
+                mov     ax, 0000h
+                mov     [200h], ax
+                ret
 
-texto:	mov 	ah, 00h
-	    mov	    al, 03h
-	    int 	10h
-	    ret
+texto:	        mov 	ah, 00h
+                mov	    al, 03h
+                int 	10h
+                ret
 
-cursor: mov	    ah, 01h
-	    mov 	ch, 00000000b
-	    mov 	cl, 00001110b
-		        ;   IRGB
-	    int 	10h
-	    ret
+cursor:         mov	    ah, 01h
+                mov 	ch, 00000000b
+                mov 	cl, 00001110b
+                        ;   IRGB
+                int 	10h
+                ret
 
-w_char:	mov 	ah, 09h
-	    mov 	al, cl
-	    mov 	bh, 0h
-	    mov 	bl, 00001111b
-	    mov 	cx, 1h
-	    int 	10h
-	    ret
+w_char:	        mov 	ah, 09h
+                mov 	al, cl
+                mov 	bh, 0h
+                mov 	bl, 00001111b
+                mov 	cx, 1h
+                int 	10h
+                ret
 
-kbwait: mov 	ax, 0000h
-	    int 	16h
-	    ret
+kbwait:         mov 	ax, 0000h
+                int 	16h
+                ret
 
-m_cursr:mov 	ah, 02h
-	    mov 	dx, di  ; columna
-	    mov 	dh, [202h] ; fila
-	    mov 	bh, 0h
-	    int 	10h
-	    ret
+m_cursr:        mov 	ah, 02h
+                mov 	dx, di  ; columna
+                mov 	dh, [202h] ; fila
+                mov 	bh, 0h
+                int 	10h
+                ret
 
-jmp_row:mov     [200h], ax
-        mov     ax, 5h
-        add     [202h], ax
-        mov     ax, [200h]
-        ret
+jmp_row:        mov     [200h], ax
+                mov     ax, 5h
+                add     [202h], ax
+                mov     ax, [200h]
+                ret
 
-linea1 :mov 	di, 25d
-lupita1:mov 	cl, [msg1+di-25d]
-	    call    m_cursr
-	    call 	w_char
-	    inc	    di
-	    cmp 	di, len1
-	    jb	    lupita1
-        call    jmp_row
-	    ret
+linea1:         mov 	di, 25d
+lupita1:        mov 	cl, [msg1+di-25d]
+                call    m_cursr
+                call 	w_char
+                inc	    di
+                cmp 	di, len1
+                jb	    lupita1
+                call    jmp_row
+                ret
 
-linea2: mov 	di, 30d
-lupita2:mov 	cl, [msg2+di-30d]
-	    call    m_cursr
-	    call 	w_char
-	    inc	    di
-	    cmp 	di, len2
-	    jb	    lupita2
-        call    jmp_row
-	    ret
+linea2:         mov 	di, 30d
+lupita2:        mov 	cl, [msg2+di-30d]
+                call    m_cursr
+                call 	w_char
+                inc	    di
+                cmp 	di, len2
+                jb	    lupita2
+                call    jmp_row
+                ret
 
-linea3: mov 	di, 15d
-lupita3:mov 	cl, [msg3+di-15d]
-	    call    m_cursr
-	    call 	w_char
-	    inc	    di
-	    cmp 	di, len3
-	    jb	    lupita3
-        call    jmp_row
-	    ret
+linea3:         mov 	di, 15d
+lupita3:        mov 	cl, [msg3+di-15d]
+                call    m_cursr
+                call 	w_char
+                inc	    di
+                cmp 	di, len3
+                jb	    lupita3
+                call    jmp_row
+                ret
 
 
 section .data
